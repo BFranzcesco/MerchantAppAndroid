8c20222a265efe9df2c43d5b06caf643286ea41b
BFranzcesco <franzb475@gmail.com>
2016-04-30 18:00:16 +0200

    uses retrofit to register gcm token

--- merchant/src/main/java/gcm/RegistrationIntentService.java
+++ merchant/src/main/java/gcm/RegistrationIntentService.java
@@ -7,10 +7,15 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
 
 import fbtm.merchantapp.R;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+
 
 public class RegistrationIntentService extends IntentService {
 
     private static final String TAG = "RegIntentService";
+    private static final String API_BASE_URL = "https://dry-badlands-78035.herokuapp.com/";
 
     public RegistrationIntentService() {
         super(TAG);
@@ -23,11 +28,20 @@ public class RegistrationIntentService extends IntentService {
             String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
             sendRegistrationToServer(token);
-        } catch (Exception e) {
-        }
+        } catch (Exception e) { }
     }
 
     private void sendRegistrationToServer(String token) {
-        //TODO Add custom implementation, as needed.
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(client)
+                .baseUrl(API_BASE_URL)
+                .build();
+
+        GcmRegistrationService service = retrofit.create(GcmRegistrationService.class);
+        service.register(token);
     }
 }
