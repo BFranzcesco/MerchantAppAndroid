593296d361f23ec1c8720e3544bc23aa01e96736
BFranzcesco <franzb475@gmail.com>
2016-06-03 19:21:10 +0200

    adds list of available pizzas; orders the choosen pizza

--- client/src/main/java/com/fbtm/clientapp/MainActivity.java
+++ client/src/main/java/com/fbtm/clientapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.fbtm.clientapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -17,12 +19,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private static final String API_BASE_URL = "https://dry-badlands-78035.herokuapp.com/";
+    public static final String[] NAMES = {"Margherita", "Pioggia", "4 Stagioni", "Wurstel", "Salamino", "Romana", "Zingara", "Crudo e gorgonzola", "Zucchine e gamberetti", "Vegetariana", "Misto bosco", "Peperoni", "Tonno e cipolle", "Bomba"};
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        listView = (ListView) findViewById(R.id.pizzas_list);
+
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, android.R.id.text1, NAMES));
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listView.setItemChecked(0, true);
     }
 
     @OnClick(R.id.order_button) void order() {
@@ -37,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         OrderService service = retrofit.create(OrderService.class);
-       Call<Void> callback = service.order();
+        String message = (String) listView.getAdapter().getItem(listView.getCheckedItemPosition());
+        Call<Void> callback = service.order(message);
         callback.enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
--- client/src/main/java/com/fbtm/clientapp/OrderService.java
+++ client/src/main/java/com/fbtm/clientapp/OrderService.java
@@ -1,8 +1,10 @@
 package com.fbtm.clientapp;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface OrderService {
-    @POST("order") Call<Void> order();
+    @FormUrlEncoded @POST("order") Call<Void> order(@Field("message") String message);
 }
